18c6327d9424c89087ffd8844a9d37b058f71f0c
Matthias Junker (u806331) <matthias.junker@mobi.ch>
2019-10-06 12:45:02 +0200

    example for not using zone.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'zone-js-talk';
+  title = "zone-js-talk";
+  value = "not yet clicked";
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  updateValue() {
+    this.value = "button was clicked";
+    this.changeDetectorRef.detectChanges();
+  }
 }

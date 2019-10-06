a92e0c9a11c7e652b9e4701c22776230dabfc98d
Matthias Junker (u806331) <matthias.junker@mobi.ch>
2019-10-06 12:52:11 +0200

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component, ChangeDetectorRef } from "@angular/core";
+import { Component, ChangeDetectorRef } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "zone-js-talk";
-  value = "not yet clicked";
+  title = 'zone-js-talk';
+  value = 'not yet clicked';
 
   constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   updateValue() {
-    this.value = "button was clicked";
+    this.value = 'button was clicked';
     this.changeDetectorRef.detectChanges();
   }
 }

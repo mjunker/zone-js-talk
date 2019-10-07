8e3c2e32b0acabafc4e53819e67c799a085e4dcd
Matthias Junker (u806331) <matthias.junker@mobi.ch>
2019-10-07 12:34:57 +0200

    example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, NgZone } from '@angular/core';
-
+declare const Zone: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,5 +8,7 @@ import { Component, NgZone } from '@angular/core';
 export class AppComponent {
   title = 'NgZone';
 
-  constructor(private ngZone: NgZone) {}
+  constructor(private ngZone: NgZone) {
+    console.log('current zone:', Zone.current.name);
+  }
 }
